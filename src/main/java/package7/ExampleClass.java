cf3ba369ec6a80d2e6512ced39b291b32f03504e
Adrian <adrian.petre@metro.digital>
2022-07-08 16:44:18 +0300

    curs7

---
+++ src/main/java/package7/ExampleClass.java
@@ -0,0 +1,19 @@
+package package7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ExampleClass {
+	
+	@Test
+	public void testCeva() {
+		System.out.println("Print test");
+		
+	System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.emag.ro/");
+	driver.quit();
+	}
+
+}
